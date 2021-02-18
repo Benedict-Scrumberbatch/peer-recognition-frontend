ab5e263f331eec4de60547004dade0354853dd01
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-02-18 15:19:50 -0500

    add routing & header to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent } from 'react';
+import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import Header from './components/Header';
 
-function App() {
+const Home: FunctionComponent<{}> = () => <h1>Home Page</h1>;
+const Foo: FunctionComponent<{}> = () => <h1>Foo Page</h1>;
+const Bar: FunctionComponent<{}> = () => <h1>Bar Page</h1>;
+const NotFound: FunctionComponent<{}> = () => <h1>Not Found</h1>;
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <div className="page">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/foo" component={Foo} />
+            <Route exact path="/bar" component={Bar} />
+            <Route path="/404" component={NotFound} />
+            <Redirect to="/404" />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
