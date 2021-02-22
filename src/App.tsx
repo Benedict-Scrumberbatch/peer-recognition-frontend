58ea50ce13dde68ba4d2216942b3e1361ffd697e
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-02-22 04:21:10 -0500

    add content minHeight so footer will be at bottom

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import React, { FunctionComponent } from 'react';
 import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
 import Header from './components/Header';
 import Login from './components/Login';
+import Footer from './components/Footer';
+
+import { makeStyles } from '@material-ui/core/styles';
+
 
 const Home: FunctionComponent<{}> = () => <h1>Home Page</h1>;
 const Foo: FunctionComponent<{}> = () => <h1>Foo Page</h1>;
 const Bar: FunctionComponent<{}> = () => <h1>Bar Page</h1>;
 const NotFound: FunctionComponent<{}> = () => <h1>Not Found</h1>;
 
+const useStyles = makeStyles((theme) => ({
+  content: {
+    minHeight: '80vh'
+  },
+}));
+
 function App () {
+  const classes = useStyles();
+  
   return (
     <div>
       <BrowserRouter>
         <Header />
-        <div className="page">
+        <div className={classes.content}>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/foo" component={Foo} />
@@ -23,6 +35,7 @@ function App () {
             <Redirect to="/404" />
           </Switch>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
