afb305a70b137366d437a460d84a89fe69cf79dd
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-08 14:01:05 -0500

    restructure into 3 files (Public, Protected, HTTPClient)

---
+++ src/api/MainAPI.tsx
@@ -0,0 +1,12 @@
+import HttpClient from './HTTPClient';
+import { User } from '../types/user';
+
+class MainApi extends HttpClient {
+  public constructor() {
+    super('https://api.awesome-site.com');
+  }
+
+  public getUsers = () => this.instance.get<User[]>('/users');
+  
+  public getUser = (id: string) => this.instance.get<User>(`/users/${id}`);
+}
