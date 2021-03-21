47ef3a44a8e38fbaa3846c745340bce8a13a56c3
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-21 13:31:06 -0400

    add signout & use localStorage

--- src/api/authHelper.tsx
+++ src/api/authHelper.tsx
@@ -2,15 +2,19 @@ const auth = {
 	isAuthenticated() {
 		if (typeof window == 'undefined') return false;
 
-		if (sessionStorage.getItem('jwt'))
-			return JSON.parse(sessionStorage.getItem('jwt') || '{}');
+		if (localStorage.getItem('jwt'))
+			return JSON.parse(localStorage.getItem('jwt') || '{}');
 		else return false;
 	},
 	authenticate(jwt: any, cb: any) {
 		if (typeof window !== 'undefined')
-			sessionStorage.setItem('jwt', JSON.stringify(jwt));
+			localStorage.setItem('jwt', JSON.stringify(jwt));
 		cb();
 	},
+	signout(cb: any) {
+		if (typeof window !== 'undefined') localStorage.removeItem('jwt');
+		cb();
+	}
 };
 
 export default auth;
