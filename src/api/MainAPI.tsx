c7f87259fd2837fa4dcb513e16882a3de6d73f39
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-22 14:50:09 -0400

    use variable based on production/development

--- src/api/MainAPI.tsx
+++ src/api/MainAPI.tsx
@@ -2,9 +2,18 @@
 import HttpClient from './HTTPClient';
 import { User } from '../types/user';
 
+let API_URL: any;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  // dev code
+  API_URL = "http://localhost:4200"
+} else {
+  API_URL = process.env.REACT_APP_API_URL;
+  // production code
+}
+
 class MainApi extends HttpClient {
   public constructor() {
-    super('http://localhost:4200');
+    super(API_URL);
   }
 
   public getUsers = () => this.instance.get<User[]>('/users');
--- src/api/MainAPIProtected.tsx
+++ src/api/MainAPIProtected.tsx
@@ -3,9 +3,19 @@ import HttpClient from './HTTPClient';
 import { AxiosRequestConfig } from 'axios';
 import { User } from '../types/user';
 
+
+let API_URL: any;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  // dev code
+  API_URL = "http://localhost:4200"
+} else {
+  API_URL = process.env.REACT_APP_API_URL;
+  // production code
+}
+
 export default class MainApiProtected extends HttpClient {
   public constructor() {
-    super('http://localhost:4200');
+    super(API_URL);
 
     this._initializeRequestInterceptor();
   }
@@ -21,7 +31,7 @@ export default class MainApiProtected extends HttpClient {
     const access_token = localStorage.getItem('access_token')
     config.headers['Authorization'] = `Bearer ${access_token}`;
     // request.headers.Authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwMTIzNDU2Nzg5IiwibmFtZSI6IlNhbXBsZSIsImlhdCI6MTUxNjIzODIzfQ.ZEBwz4pWYGqgFJc6DIi7HdTN0z5Pfs4Lcv4ZNwMr1rs';
-  
+
     return config;
   };
 
