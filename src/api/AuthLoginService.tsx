b01d5168c1e3d560df1d370a0eb375bbd8f98297
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-04-18 21:13:11 -0400

    Update AuthLoginService.tsx

--- src/api/AuthLoginService.tsx
+++ src/api/AuthLoginService.tsx
@@ -1,10 +1,14 @@
-import MainApiProtected from "./MainAPIProtected";
+import MainApi from './MainAPI';
+import { Tag } from '../dtos/entity/tag.entity';
 
-export default class AuthLoginService extends MainApiProtected {
+export default class AuthLoginService extends MainApi {
     public constructor() {
         super();
     }
     public postLogin = (username: any, password: any) => {
         return this.instance.post('/auth/login', {'username': username, 'password': password});
     };
+    public postCreateCompany = (name: any, tags: Tag[]) => {
+        return this.instance.post('/createCompany', {'name': name, 'tags': tags});
+    }
 }
