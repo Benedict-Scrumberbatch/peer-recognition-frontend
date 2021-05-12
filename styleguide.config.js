92447d9ae35757ca98b0032064672bba46deb915
Sahil Malhotra <sahilmalhotra164@gmail.com>
2021-05-12 01:05:13 -0400

    react styleguide compiler config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,29 @@
 module.exports = {
 	components: 'src/components/**/[A-Z]*.tsx', 
 	ignore: ['**/src/components/Feed/Feed.tsx','**/*.test.{js,jsx,ts,tsx}','**src/components/Private/PrivateNavbar.tsx'
-	,'src/components/Private/PrivateRoute.tsx','**/src/components/Signup/Signup.tsx', "**/src/components/Signup/TagSelect.tsx"]
+	,'src/components/Private/PrivateRoute.tsx','**/src/components/Signup/Signup.tsx', "**/src/components/Signup/TagSelect.tsx"],
+	compilerConfig: {
+		"target": "es5",
+		"lib": [
+		  "dom",
+		  "dom.iterable",
+		  "esnext"
+		],
+		"allowJs": true,
+		"skipLibCheck": true,
+		"esModuleInterop": true,
+		"allowSyntheticDefaultImports": true,
+		"strict": false,
+		"strictPropertyInitialization": false,
+		"forceConsistentCasingInFileNames": true,
+		"noFallthroughCasesInSwitch": true,
+		"module": "esnext",
+		"moduleResolution": "node",
+		"resolveJsonModule": true,
+		"isolatedModules": true,
+		"noEmit": true,
+		"jsx": "react-jsx",
+		"experimentalDecorators": true,
+		"emitDecoratorMetadata": true,
+	  },
 }
