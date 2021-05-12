8d12d74451e30d02f65de668ec1a6118516418fe
Sahil Malhotra <sahilmalhotra164@gmail.com>
2021-05-12 02:41:19 -0400

    fixed (?) auto doc

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,39 +1,41 @@
+var path = require('path')
+var webpack = require('webpack');
+
 module.exports = {
 	components: 'src/components/**/[A-Z]*.tsx', 
 	ignore: ['**/src/components/Feed/Feed.tsx','**/*.test.{js,jsx,ts,tsx}','**src/components/Private/PrivateNavbar.tsx'
 	,'src/components/Private/PrivateRoute.tsx','**/src/components/Signup/Signup.tsx', "**/src/components/Signup/TagSelect.tsx"],
-	compilerConfig: {
-		target: "es5",
-		lib: [
-		  "dom",
-		  "dom.iterable",
-		  "esnext"
-		],
-		allowJs: true,
-		skipLibCheck: true,
-		esModuleInterop: true,
-		allowSyntheticDefaultImports: true,
-		strict: false,
-		strictPropertyInitialization: false,
-		forceConsistentCasingInFileNames: true,
-		noFallthroughCasesInSwitch: true,
-		module: "esnext",
-		moduleResolution: "node",
-		resolveJsonModule: true,
-		isolatedModules: true,
-		noEmit: true,
-		jsx: "react-jsx",
-		experimentalDecorators: true,
-		emitDecoratorMetadata: true,
-	  },
 	  webpackConfig: {
+		plugins: [
+			new webpack.ProvidePlugin({
+			  process: 'process/browser',
+			}),
+		  ],
 		module: {
 			rules: [
 				{
-					test: /\.tsx?$/,
-					exclude: /node_modules/,
-					loader: 'babel-loader',
-				},
+					test: /\.(png|jpe?g|gif)$/i,
+					use: [
+					  {
+						loader: 'file-loader',
+					  },
+					],
+				  },
+				{
+                    test: /\.(js|jsx|tsx|ts)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                        },
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                configFile: path.resolve('./tsconfig.json'),
+                            },
+                        },
+                    ],
+                },
 				{
 					test: /\.css$/,
 					use: ['style-loader', 'css-loader'],
