29a05258c683625ea1fe7cc75186f911991bc464
Sahil Malhotra <sahilmalhotra164@gmail.com>
2021-05-12 05:26:56 -0400

    github pages

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,12 +1,14 @@
 var path = require('path')
 var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+
 
 module.exports = {
 	components: 'src/components/**/[A-Z]*.tsx', 
 	ignore: ['**/tools/postinstall.js', '**/src/components/Feed/Feed.tsx','**/*.test.{js,jsx,ts,tsx}','**src/components/Private/PrivateNavbar.tsx'
 	,'src/components/Private/PrivateRoute.tsx','**/src/components/Signup/Signup.tsx', "**/src/components/Signup/TagSelect.tsx"],
 	  webpackConfig: {
-		context: path.resolve('./src'),
+		externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
 		plugins: [
 			new webpack.ProvidePlugin({
 			  process: 'process/browser',
@@ -24,6 +26,7 @@ module.exports = {
 				  },
 				{
                     test: /\.(jsx|js|tsx|ts)$/,
+                    exclude: /node_modules/,
                     use: [
                         {
                             loader: 'babel-loader',
